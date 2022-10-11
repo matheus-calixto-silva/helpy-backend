f02b552eba9fff0b335fcbd86f8ee2647ed6f90a
Matheus Calixto <matheuscalixto8@gmail.com>
2022-10-10 22:48:14 -0300

    refactor(config): ajuste nas variaveis de ambiente

--- utils/config.js
+++ utils/config.js
@@ -1,12 +1,12 @@
-require('dotenv').config;
+require('dotenv').config();
 
 const PORT = process.env.PORT;
-const MONGODBURI =
+const MONGODB_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODBURI;
+    : process.env.MONGODB_URI;
 
 module.exports = {
-  MONGODBURI,
+  MONGODB_URI,
   PORT,
 };
