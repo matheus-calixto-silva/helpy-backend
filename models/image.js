b63df4176084267228ef6215e581e49debcaa736
Matheus Calixto <matheuscalixto8@gmail.com>
2022-10-11 14:46:11 -0300

    feat(image): schema de imagem

---
+++ models/image.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const aws = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+
+const s3 = new aws.S3();
+
+const imageSchema = new mongoose.Schema({
+  name: String,
+  size: Number,
+  key: String,
+  url: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+imageSchema.pre('save', function () {
+  if (!this.url) {
+    this.url = `http://localhost:3000/${process.env.PORT}/files/${this.key}`;
+  }
+});
+
+imageSchema.pre('remove', function () {
+  if (process.env.STORAGE_TYPE === 's3') {
+    return s3
+      .deleteObject({
+        Bucket: process.env.BUCKET_NAME,
+        Key: this.key,
+      })
+      .promise()
+      .then((response) => {
+        console.log(response.status);
+      })
+      .catch((response) => {
+        console.log(response.status);
+      });
+  } else {
+    return promisify(fs.unlink)(
+      path.resolve(__dirname, '..', '..', 'tmp', 'uploads', this.key)
+    );
+  }
+});
+
+imageSchema.set('toJSON', {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Image = mongoose.model('Image', imageSchema);
+
+module.exports = Image;
