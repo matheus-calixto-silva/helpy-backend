9814369e5507e13676ed051e7d0d559602e55b7d
Matheus Calixto <matheuscalixto8@gmail.com>
2022-10-11 15:52:57 -0300

    feat(users): controlador de usuário

---
+++ controllers/users.js
@@ -0,0 +1,76 @@
+const bcrypt = require('bcrypt');
+const usersRouter = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('../utils/multer');
+const User = require('../models/user');
+const Image = require('../models/image');
+
+const onePhotoConfig = multer(multerConfig).single('photo');
+
+usersRouter.get('/', async (_request, response) => {
+  const users = await User.find({}).populate('profilePic', { url: 1 });
+  response.status(200).json(users);
+});
+
+usersRouter.post('/', onePhotoConfig, async (request, response) => {
+  const { firstName, lastName, username, email, password, phone } =
+    request.body;
+
+  const { originalname: name, size, key, location: url = '' } = request.file;
+
+  const existingUser = await User.findOne({ username });
+
+  if (existingUser) {
+    return response.status(400).json({
+      error: 'nome de usuário deve ser único',
+    });
+  }
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    firstName,
+    lastName,
+    username,
+    email,
+    passwordHash,
+    phone,
+  });
+
+  const savedUser = await user.save();
+  const { _id } = savedUser;
+
+  const image = await Image.create({
+    name,
+    size,
+    key,
+    url,
+    user: {
+      _id,
+    },
+  });
+
+  savedUser.profilePic = image._id;
+
+  await User.findByIdAndUpdate(savedUser._id.toString(), savedUser, {
+    new: true,
+  });
+
+  response.status(201).json(savedUser);
+});
+
+usersRouter.get('/:id', async (request, response) => {
+  const user = await User.findById(request.params.id);
+  response.status(200).json(user);
+});
+
+
+usersRouter.delete('/:id', async (request, response) => {
+  const user = await User.findById(request.params.id);
+  await user.remove();
+
+  response.status(204).send();
+});
+
+module.exports = usersRouter;
