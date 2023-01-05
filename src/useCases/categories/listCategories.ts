0114ea8baf45a565b9c675c9c00dd15d824d39d4
Matheus Calixto <matheuscalixto8@gmail.com>
2023-01-05 11:00:39 -0300

    feat(usecases): created function responsible to list all categories

---
+++ src/useCases/categories/listCategories.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+import { Category } from '../../models/category';
+
+export const listCategories = async(req: Request, res: Response) => {
+  const categories = await Category.find();
+
+  return res.status(200).json(categories);
+};
