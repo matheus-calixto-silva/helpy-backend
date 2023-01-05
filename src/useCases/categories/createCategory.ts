7b1a219c4ec499db62fa6a25dfce03c2cfd4d6f7
Matheus Calixto <matheuscalixto8@gmail.com>
2023-01-05 11:00:06 -0300

    feat(usecases): created function responsible to create a new category

---
+++ src/useCases/categories/createCategory.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+import { Category } from '../../models/category';
+
+export const createCategory = async (req: Request, res: Response) => {
+  const { name, description } = req.body;
+  const category = await Category.create({ name, description });
+
+  res.status(201).json(category);
+};
