c722dc8644cb16d1757d7a41fb768d508747a61e
Matheus Calixto <matheuscalixto8@gmail.com>
2023-01-25 13:57:41 -0300

    feat(usecases/categories): created function responsible to update a category

---
+++ src/useCases/categories/updateCategory.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+
+import { Category } from '../../models/category';
+
+export const updateCategory = async (req: Request, res: Response) => {
+  const { categoryId } = req.params;
+  const updates = req.body;
+
+  const updatedCategory = await Category.findByIdAndUpdate(categoryId, updates, { new: true });
+
+  if (!updatedCategory) {
+    return res.status(404).send({ error: 'Category not found' });
+  }
+
+  return res.status(200).json(updatedCategory);
+};
