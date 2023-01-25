69c4d87239dd3903a6641f31f34226496e7452dc
Matheus Calixto <matheuscalixto8@gmail.com>
2023-01-24 21:36:44 -0300

    feat(controllers): create route to remove a category

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { createCategory } from '../useCases/categories/createCategory';
+import { removeCategory } from '../useCases/categories/removeCategory';
 import { listCategories } from '../useCases/categories/listCategories';
 
 export const categoriesRouter = Router();
@@ -7,3 +8,5 @@ export const categoriesRouter = Router();
 categoriesRouter.get('/categories', listCategories);
 
 categoriesRouter.post('/categories', createCategory);
+
+categoriesRouter.delete('/categories/:categoryId', removeCategory);
