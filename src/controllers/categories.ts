6e87a3c747e31bdb9d4047ccccf22aeb5f0cbbb8
Matheus Calixto <matheuscalixto8@gmail.com>
2023-01-24 22:29:54 -0300

    feat(controllers): created route to edit a category

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { createCategory } from '../useCases/categories/createCategory';
+import { updateCategory } from '../useCases/categories/updateCategory';
 import { removeCategory } from '../useCases/categories/removeCategory';
 import { listCategories } from '../useCases/categories/listCategories';
 
@@ -8,5 +9,7 @@ export const categoriesRouter = Router();
 categoriesRouter.get('/categories', listCategories);
 
 categoriesRouter.post('/categories', createCategory);
+
+categoriesRouter.put('/categories/:categoryId', updateCategory);
 
 categoriesRouter.delete('/categories/:categoryId', removeCategory);
