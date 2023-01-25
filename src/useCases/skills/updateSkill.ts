c5d11b97b9a4fa80dfeab776a5fb483f982eb256
Matheus Calixto <matheuscalixto8@gmail.com>
2023-01-25 14:52:13 -0300

    feat(usecases/skills): created function responsible to update a skill

---
+++ src/useCases/skills/updateSkill.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+
+import { Skill } from '../../models/skill';
+
+export const updateSkill = async (req: Request, res: Response) => {
+  const { skillId } = req.params;
+  const updates = req.body;
+
+  const updatedSkill = await Skill.findByIdAndUpdate(skillId, updates, { new: true });
+
+  if (!updatedSkill) {
+    return res.status(404).send({ error: 'Category not found' });
+  }
+
+  res.status(200).json(updatedSkill);
+};
