a72eaf6d8b49c710a0b2c4d36d055e26fc4d5303
Matheus Calixto <matheuscalixto8@gmail.com>
2023-01-26 09:04:09 -0300

    feat(usecases/ongs): created function responsible to create an ong event

---
+++ src/useCases/ongs/createOngEvent.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+
+import { Ong } from '../../models/ong';
+import { Event } from '../../models/event';
+
+export const createOngEvent = async (req: Request, res: Response) => {
+  const { ongId } = req.params;
+  const { name, local, date, description, requiredSkills, maxVolunteers } = req.body;
+
+  const parsedSkills = requiredSkills.split(',').filter(Boolean);
+
+  const ong = await Ong.findById(ongId);
+
+  const newEvent = {
+    name,
+    local,
+    date,
+    description,
+    requiredSkills: parsedSkills,
+    maxVolunteers,
+    volunteers: [],
+  };
+
+  const event = await Event.create(newEvent);
+
+  const updatedEvents = ong?.events ? [...ong.events, event._id] : [event._id];
+  const updatedOng = await Ong.findByIdAndUpdate(ongId, { events: updatedEvents }, { new: true });
+
+  return res.status(200).json(updatedOng);
+};
