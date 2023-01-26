9250d83abd32bbc9789bb81d2e033626a9d59614
Matheus Calixto <matheuscalixto8@gmail.com>
2023-01-26 09:18:59 -0300

    feat(usecases/ongs): created function responsible to list all ong events

---
+++ src/useCases/ongs/listEventsByOng.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+
+import { Ong } from '../../models/ong';
+
+export const listEventsByOng = async (req: Request, res: Response) => {
+  const { ongId } = req.params;
+  const ong = await Ong.findById(ongId).populate({
+    path: 'events',
+    populate: { path: 'requiredSkills' }
+  });
+
+  return res.status(200).json(ong?.events);
+};
