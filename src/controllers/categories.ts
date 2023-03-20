413f081826e60265fb24a3ecf9d9cd18dea60684
Matheus Calixto <matheuscalixto8@gmail.com>
2023-03-20 09:36:21 -0300

    feat(controllers): created function responsible to get a category by id

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -3,12 +3,15 @@ import { createCategory } from '../useCases/categories/createCategory';
 import { updateCategory } from '../useCases/categories/updateCategory';
 import { removeCategory } from '../useCases/categories/removeCategory';
 import { listCategories } from '../useCases/categories/listCategories';
+import { getCategoryById } from '../useCases/categories/getCategoryById';
 
 export const categoriesRouter = Router();
 
 categoriesRouter.get('/categories', listCategories);
 
 categoriesRouter.post('/categories', createCategory);
+
+categoriesRouter.get('/categories/:categoryId', getCategoryById);
 
 categoriesRouter.put('/categories/:categoryId', updateCategory);
 
