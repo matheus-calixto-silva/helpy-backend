0c76d740006f73dd93eea39057ae82b3785932ce
Matheus Calixto <matheuscalixto8@gmail.com>
2023-03-20 10:46:12 -0300

    refactor(usecases/ongs): rename function deleteOngEvent to removeOngEvent

---
+++ src/useCases/ongs/removeOngEvent.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+
+import { Ong } from '../../models/ong';
+import { Event } from '../../models/event';
+
+export const removeOngEvent = async (req: Request, res: Response) => {
+  const { ongId, eventId } = req.params;
+
+  const eventToRemove = await Event.findById(eventId);
+
+  if (eventToRemove) {
+    const ong = await Ong.findById(ongId);
+    const filteredOngEvents = ong?.events.filter(event => event._id !== eventToRemove._id);
+
+    await Event.findByIdAndDelete(eventId);
+    await Ong.findByIdAndUpdate(ongId, { events: filteredOngEvents }, { new: true });
+
+    const updatedOng = await Ong.findById(ongId).populate({
+      path: 'events',
+      populate: [
+        { path: 'requiredSkills' },
+        { path: 'volunteers' }
+      ]
+    });
+
+    return res.status(200).json(updatedOng?.events);
+  } else {
+    return res.status(500).send({ error: 'Internal server error' });
+  }
+};
