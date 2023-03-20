c589d74de4e2d1bdb6ef60d1f5827a3fe3953f1b
Matheus Calixto <matheuscalixto8@gmail.com>
2023-03-20 10:07:05 -0300

    feat(usecases): created function responsible to create a skill

---
+++ src/useCases/skills/createSkill.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+import { Skill } from '../../models/skill';
+
+export const createSkill = async (req: Request, res: Response) => {
+  const { name, category } = req.body;
+  const skill = await Skill.create({ name, category });
+
+  res.status(201).json(skill);
+};
