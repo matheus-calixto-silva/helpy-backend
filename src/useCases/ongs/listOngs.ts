e1a3173ccedc3d52f4e8957bba06a829cbced090
Matheus Calixto <matheuscalixto8@gmail.com>
2023-03-20 11:40:08 -0300

    feat(usecases/ongs): updated function to list volunteers data

--- src/useCases/ongs/listOngs.ts
+++ src/useCases/ongs/listOngs.ts
@@ -5,7 +5,10 @@ import { Ong } from '../../models/ong';
 export const listOngs = async (req: Request, res: Response) => {
   const ongs = await Ong.find().populate({
     path: 'events',
-    populate: { path: 'requiredSkills' }
+    populate: [
+      { path: 'requiredSkills' },
+      { path: 'volunteers' }
+    ]
   });
 
   return res.status(200).json(ongs);
