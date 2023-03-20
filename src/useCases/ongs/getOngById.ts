71c9505ae57bed5bc3dd9bcd1c4a2a0da3e0f632
Matheus Calixto <matheuscalixto8@gmail.com>
2023-03-20 10:32:45 -0300

    feat(usecases/ongs): created function responsible to get an ong by id

---
+++ src/useCases/ongs/getOngById.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { Ong } from '../../models/ong';
+
+export const getOngById = async (req: Request, res: Response) => {
+  const { ongId } = req.params;
+  const ong = await Ong.findById(ongId).populate({
+    path: 'events',
+    populate: [
+      { path: 'requiredSkills' },
+      { path: 'volunteers' }
+    ]
+  });
+
+  return res.status(200).json(ong);
+};
