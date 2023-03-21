56892934f3863104e75672ff07e5b7510fcbac61
Matheus Calixto <matheuscalixto8@gmail.com>
2023-03-21 15:49:28 -0300

    fix(usecases/ongs): fixed function responsible to create a new ong event
    an event can only be added if the ong is found in the database

--- src/useCases/ongs/createOngEvent.ts
+++ src/useCases/ongs/createOngEvent.ts
@@ -11,29 +11,33 @@ export const createOngEvent = async (req: Request, res: Response) => {
 
   const ong = await Ong.findById(ongId);
 
-  const newEvent = {
-    name,
-    local,
-    date,
-    description,
-    requiredSkills: parsedSkills,
-    maxVolunteers,
-    volunteers: [],
-  };
+  if (ong) {
+    const newEvent = {
+      name,
+      local,
+      date,
+      description,
+      requiredSkills: parsedSkills,
+      maxVolunteers,
+      volunteers: [],
+    };
 
-  const event = await Event.create(newEvent);
+    const event = await Event.create(newEvent);
 
-  const updatedEvents = ong?.events ? [...ong.events, event._id] : [event._id];
+    const updatedEvents = ong?.events ? [...ong.events, event._id] : [event._id];
 
-  await Ong.findByIdAndUpdate(ongId, { events: updatedEvents }, { new: true });
+    await Ong.findByIdAndUpdate(ongId, { events: updatedEvents }, { new: true });
 
-  const ongWithPopulatedData = await Ong.findById(ongId).populate({
-    path: 'events',
-    populate: [
-      { path: 'requiredSkills', populate: { path: 'category' } },
-      { path: 'volunteers' }
-    ]
-  });
+    const ongWithPopulatedData = await Ong.findById(ongId).populate({
+      path: 'events',
+      populate: [
+        { path: 'requiredSkills', populate: { path: 'category' } },
+        { path: 'volunteers' }
+      ]
+    });
 
-  return res.status(200).json(ongWithPopulatedData);
+    return res.status(200).json(ongWithPopulatedData);
+  } else {
+    return res.status(404).send({ error: 'Error: ONG not found' });
+  }
 };
