64ebbdbf5426c571659e65ac52722a82da8ba0b2
Matheus Calixto <matheuscalixto8@gmail.com>
2023-03-30 10:39:38 -0300

    feat(usecases/users): added check if user wants to change his password

--- src/useCases/users/updateUser.ts
+++ src/useCases/users/updateUser.ts
@@ -1,16 +1,31 @@
 import { Request, Response } from 'express';
 
 import { User } from '../../models/user';
+import { IUser } from '../../types';
+
+import { genNewPasswordHash } from '../../utils/helpers';
 
 export const updateUser = async (req: Request, res: Response) => {
   const { userId } = req.params;
-  const updates = req.body;
+  const updates: IUser = req.body;
+  let obj = {};
 
-  const updatedUser = await User.findByIdAndUpdate(userId, updates, { new: true });
+  if (updates) {
+    if (updates.password) {
+      const newPasswordHash = await genNewPasswordHash(updates.password);
+      updates.passwordHash = newPasswordHash;
 
-  if (!updatedUser) {
-    return res.status(404).send({ error: 'User not found' });
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...withoutPassword } = updates;
+      obj = { ...withoutPassword };
+
+      const updatedUserWithPassword = await User.findByIdAndUpdate(userId, obj, { new: true });
+      return res.status(200).json(updatedUserWithPassword);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(userId, updates, { new: true });
+    return res.status(200).json(updatedUser);
   }
 
-  return res.status(200).json(updatedUser);
+  return res.status(404).send({ error: 'User not found' });
 };
