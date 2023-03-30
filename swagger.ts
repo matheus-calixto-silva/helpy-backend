7e01833422d9240f768972c529c6cb5e84fbfc83
Matheus Calixto <matheuscalixto8@gmail.com>
2023-03-30 14:01:01 -0300

    feat: set default props and values in swagger docs

--- swagger.ts
+++ swagger.ts
@@ -3,4 +3,27 @@ import swaggerAutogen from 'swagger-autogen';
 const outputFile = './swagger_output.json';
 const endpointsFiles = ['./src/controllers/**/*.ts'];
 
-swaggerAutogen()(outputFile, endpointsFiles);
+const doc = {
+  info: {
+    version: '1.0.0',
+    title: 'Helpy API Documentation',
+    description: 'Helpy REST API Documentation'
+  },
+  host: 'localhost:3001',
+  basePath: '/',
+  schemes: ['http'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  securityDefinitions: {
+    bearerAuth: {
+      type: 'apiKey',
+      name: 'Authorization',
+      in: 'header'
+    }
+  },
+  security: [
+    { bearerAuth: [] }
+  ]
+};
+
+swaggerAutogen()(outputFile, endpointsFiles, doc);
