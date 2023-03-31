1a4618e301cfc877bc1e1e9be6a041a5a1c89a79
Matheus Calixto <matheuscalixto8@gmail.com>
2023-03-31 17:02:27 -0300

    feat: added user object on doc definitions and update port

--- swagger.ts
+++ swagger.ts
@@ -1,4 +1,5 @@
 import swaggerAutogen from 'swagger-autogen';
+import { PORT } from './src/utils/config';
 
 const outputFile = './swagger_output.json';
 const endpointsFiles = ['./src/controllers/**/*.ts'];
@@ -9,7 +10,7 @@ const doc = {
     title: 'Helpy API Documentation',
     description: 'Helpy REST API Documentation'
   },
-  host: 'localhost:3001',
+  host: `localhost:${PORT}`,
   basePath: '/',
   schemes: ['http'],
   consumes: ['application/json'],
@@ -23,7 +24,37 @@ const doc = {
   },
   security: [
     { bearerAuth: [] }
-  ]
+  ],
+  definitions: {
+    User: {
+      _id: '63e52122b00d0c03dfc5304d',
+      firstname: 'Jhon',
+      lastname: 'Doe',
+      username: 'john_doe',
+      email: 'john_doe@mail.com',
+      phone: '5581988888888',
+      skills: [
+        {
+          skill: {
+            _id: '6419f3540d17fe8c1b530ab6',
+            name: 'Desenvolvimento React',
+            category: {
+              _id: '6419f19a0d17fe8c1b530aa8',
+              name: 'Programação',
+              description: 'Habilidades relacionadas a desenvolvimento de software',
+              created_at: '2023-03-21T18:04:10.044Z',
+              updated_at: '2023-03-21T18:04:10.044Z'
+            },
+            created_at: '2023-03-21T18:11:32.357Z',
+            updated_at: '2023-03-21T18:11:32.357Z'
+          },
+          _id: '6424915edd50adc8b28f4dd7'
+        }
+      ],
+      created_at: '2023-03-29T19:28:31.002Z',
+      updated_at: '2023-03-29T19:28:31.002Z'
+    },
+  }
 };
 
 swaggerAutogen()(outputFile, endpointsFiles, doc);
