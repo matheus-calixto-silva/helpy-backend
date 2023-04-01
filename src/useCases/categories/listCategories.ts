64242dd6a6b8ef8d0e3cccc026965aa39382ea22
Matheus Calixto <matheuscalixto8@gmail.com>
2023-04-01 11:19:59 -0300

    refactor(usecases/categories): marked req parameter as not used

--- src/useCases/categories/listCategories.ts
+++ src/useCases/categories/listCategories.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 import { Category } from '../../models/category';
 
-export const listCategories = async(req: Request, res: Response) => {
+export const listCategories = async (_req: Request, res: Response) => {
   const categories = await Category.find();
 
   return res.status(200).json(categories);
