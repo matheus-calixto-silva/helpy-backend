81f0a577a8f5c10d3d6fca84769fb40070bab9ad
Matheus Calixto <matheuscalixto8@gmail.com>
2023-04-01 14:59:37 -0300

    feat(usecases/admins): created function responsible to list all admins

---
+++ src/useCases/admins/listAdmins.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+import { Admin } from '../../models/admin';
+
+export const listAdmins = async (_req: Request, res: Response) => {
+  const admins = await Admin.find();
+
+  return res.status(200).json(admins);
+};
