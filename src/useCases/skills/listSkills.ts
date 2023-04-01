b0dbb1cae90468cb4afdea7ed911480c4e19d25f
Matheus Calixto <matheuscalixto8@gmail.com>
2023-04-01 12:13:40 -0300

    refactor(usecases/skills): marked req parameter as not used on listSkills

--- src/useCases/skills/listSkills.ts
+++ src/useCases/skills/listSkills.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 import { Skill } from '../../models/skill';
 
-export const listSkills = async(req: Request, res: Response) => {
+export const listSkills = async(_req: Request, res: Response) => {
   const skills = await Skill.find().populate('category');
 
   return res.status(200).json(skills);
