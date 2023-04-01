e1d9aab88f7fdbaee5015aa5fa50b47d5952a7b0
Matheus Calixto <matheuscalixto8@gmail.com>
2023-03-31 22:28:06 -0300

    refactor(utils): added upload function used on user and ong controllers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,6 @@
+import path from 'node:path';
 import bcrypt from 'bcrypt';
+import multer from 'multer';
 
 export const genNewPasswordHash = (newPassword: string) => {
   const saltRounds = 10;
@@ -6,3 +8,15 @@ export const genNewPasswordHash = (newPassword: string) => {
 
   return passwordHash;
 };
+
+export const upload = multer({
+  storage: multer.diskStorage({
+    destination(_req, _file, callback) {
+      callback(null, path.resolve('uploads'));
+    },
+    filename(_req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`);
+    },
+  }
+  ),
+});
