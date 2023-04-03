31056108aeb1e5e3330ae4be89ecd88c4f668a20
Matheus Calixto <matheuscalixto8@gmail.com>
2023-04-03 15:13:33 -0300

    docs(usecases/users): added comments to help swagger generation on removeUser function

--- src/useCases/users/removeUser.ts
+++ src/useCases/users/removeUser.ts
@@ -5,6 +5,10 @@ import fs from 'node:fs';
 import { User } from '../../models/user';
 
 export const removeUser = async (req: Request, res: Response) => {
+  // #swagger.tags = ['User']
+  // #swagger.description = 'Endpoint para remove um usuário.'
+  // #swagger.parameters['userId'] = { description: 'ID do usuário.' }
+
   const { userId } = req.params;
 
   const user = await User.findById(userId);
@@ -15,7 +19,11 @@ export const removeUser = async (req: Request, res: Response) => {
       if (err) throw err;
       console.log('File deleted!');
     });
+
+    // #swagger.responses[204] = { description: 'Usuário removido' }
+    return res.sendStatus(204);
   }
 
-  res.sendStatus(204);
+  // #swagger.responses[404] = { description: 'Usuário não encontrado' }
+  return res.status(404).send({ error: 'User not found' });
 };
