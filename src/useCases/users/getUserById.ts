0f1ccc7b830eee0767d33612391b6b96f9da6040
Matheus Calixto <matheuscalixto8@gmail.com>
2023-04-03 15:35:32 -0300

    docs(usecases/users): added comments to help swagger generation on getUserById function

--- src/useCases/users/getUserById.ts
+++ src/useCases/users/getUserById.ts
@@ -2,6 +2,10 @@ import { Request, Response } from 'express';
 import { User } from '../../models/user';
 
 export const getUserById = async (req: Request, res: Response) => {
+  // #swagger.tags = ['User']
+  // #swagger.description = 'Endpoint para obter um usuário.'
+  // #swagger.parameters['userId'] = { description: 'ID do usuário.' }
+
   const { userId } = req.params;
   const user = await User.findById(userId).populate({
     path: 'skills.skill',
@@ -11,8 +15,13 @@ export const getUserById = async (req: Request, res: Response) => {
   });
 
   if (user) {
+    /* #swagger.responses[200] = {
+       schema: { $ref: "#/definitions/User" },
+       description: 'Usuário encontrado'
+} */
     return res.status(200).json(user);
   }
 
+  // #swagger.responses[404] = { description: 'Usuário não encontrado' }
   return res.status(404).send({ error: 'User not found' });
 };
