f1a0f0b304819c818127e1c1838d217a124c5058
Matheus Calixto <matheuscalixto8@gmail.com>
2023-04-04 15:50:52 -0300

    feat(usecases/users): added route to update an user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -5,6 +5,7 @@ import { listUsers } from '../useCases/users/listUsers';
 import { removeUser } from '../useCases/users/removeUser';
 import { updateUserSkills } from '../useCases/users/updateUserSkills';
 import { getUserById } from '../useCases/users/getUserById';
+import { updateUser } from '../useCases/users/updateUser';
 
 export const usersRouter = Router();
 
@@ -15,6 +16,8 @@ usersRouter.get('/users', listUsers);
 usersRouter.post('/users', upload.single('photo'), createUser);
 
 usersRouter.get('/users/:userId', getUserById);
+
+usersRouter.put('/users/:userId', updateUser);
 
 usersRouter.patch('/users/:userId', updateUserSkills);
 
