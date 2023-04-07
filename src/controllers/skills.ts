300573cee39ec41323dc890731b2fb036c2da6e1
Matheus Calixto <matheuscalixto8@gmail.com>
2023-04-07 18:47:46 -0300

    feat(controllers/skills): added custom middlewares to skills router

--- src/controllers/skills.ts
+++ src/controllers/skills.ts
@@ -5,14 +5,16 @@ import { removeSkill } from '../useCases/skills/removeSkill';
 import { listSkills } from '../useCases/skills/listSkills';
 import { getSkillById } from '../useCases/skills/getSkillById';
 
+import { auth, authAdmin } from '../utils/middleware';
+
 export const skillsRouter = Router();
 
-skillsRouter.get('/skills', listSkills);
+skillsRouter.get('/skills', auth, listSkills);
 
-skillsRouter.post('/skills', createSkill);
+skillsRouter.post('/skills', authAdmin, createSkill);
 
-skillsRouter.get('/skills/:skillId', getSkillById);
+skillsRouter.get('/skills/:skillId', auth, getSkillById);
 
-skillsRouter.put('/skills/:skillId', updateSkill);
+skillsRouter.put('/skills/:skillId', authAdmin, updateSkill);
 
-skillsRouter.delete('/skills/:skillId', removeSkill);
+skillsRouter.delete('/skills/:skillId', authAdmin, removeSkill);
