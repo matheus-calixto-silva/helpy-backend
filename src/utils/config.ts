583106407c54c0f5c59141455b417bff9e5f21ec
Matheus Calixto <matheuscalixto8@gmail.com>
2023-04-25 16:20:33 -0300

    feat(utils/config.ts): created constant to config nodemailer service

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,13 @@
 import 'dotenv/config';
+import { createTransport } from 'nodemailer';
 
 export const PORT = process.env.PORT;
 export const MONGODB_URI = process.env.MONGODB_URI ?? '';
+
+export const transporter = createTransport({
+  service: process.env.MAIL_SERVICE as string,
+  auth: {
+    user: process.env.MAIL_USER as string,
+    pass: process.env.MAIL_PASSWORD as string
+  }
+});
