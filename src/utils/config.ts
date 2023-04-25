7b3568889267b913c12149e28629894a669c5628
Matheus Calixto <matheuscalixto8@gmail.com>
2023-04-25 16:24:29 -0300

    refactor(utils/config.ts): refactored MONGODB_URI constant

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { createTransport } from 'nodemailer';
 
 export const PORT = process.env.PORT;
-export const MONGODB_URI = process.env.MONGODB_URI ?? '';
+export const MONGODB_URI = process.env.MONGODB_URI as string;
 export const SECRET_KEY = process.env.SECRET as string;
 
 export const transporter = createTransport({
