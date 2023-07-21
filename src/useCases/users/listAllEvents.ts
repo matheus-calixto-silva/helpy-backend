e6053fbfa8765304342ec0edd28217e0a8be77d5
Matheus Calixto <matheuscalixto8@gmail.com>
2023-07-20 21:59:14 -0300

    feat(usecases/users): created listAllEvents method

---
+++ src/useCases/users/listAllEvents.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import {Types} from 'mongoose';
+
+import { Ong } from '../../models/ong';
+
+export const listAllEvents = async (_req: Request, res: Response) => {
+  const ongs = await Ong.find().populate({
+    path: 'events',
+    populate: [
+      { path: 'requiredSkills', populate: { path: 'category' } },
+      { path: 'volunteers' }
+    ]
+  });
+
+  if (ongs) {
+    const allEvents = ongs.reduce((acc: Types.ObjectId[], ong) => {
+      if (ong.events && ong.events.length > 0) {
+        acc.push(...ong.events);
+      }
+      return acc;
+    }, []);
+
+    return res.status(200).json(allEvents);
+  }
+
+  return res.status(500).json({ error: 'Failed to fetch events from all ONGs.' });
+};
