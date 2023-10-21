962620891f29e45ba8f6744ede5460c5233fdfb3
Matheus Calixto <matheuscalixto8@gmail.com>
2023-10-21 16:17:43 -0300

    feat(usecases/ongs): created a method responsible to update an ong

---
+++ src/useCases/ongs/updateOngEvent.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from 'express';
+
+import { Event } from '../../models/event';
+
+export const updateOngEvent = async (req: Request, res: Response) => {
+  const { ongId, eventId } = req.params;
+  const eventPic = req.file?.filename;
+  const hasIds = ongId && eventId;
+  const {
+    name,
+    street,
+    city,
+    uf,
+    latitude,
+    longitude,
+    date,
+    description,
+    skills,
+    volunteers,
+    maxVolunteers } = req.body;
+
+  const parsedSkills = skills.split(',').map((skill: string) => skill.trim());
+
+  if (hasIds) {
+    const address = {
+      street,
+      city,
+      uf,
+      latitude,
+      longitude,
+    };
+
+    const eventToUpdate = {
+      name,
+      address,
+      date,
+      description,
+      requiredSkills: parsedSkills,
+      maxVolunteers,
+      eventPic,
+      volunteers,
+    };
+
+
+    if (eventPic) {
+      eventToUpdate.eventPic = eventPic;
+    }
+
+    await Event.findByIdAndUpdate(eventId, eventToUpdate, { new: true });
+
+    const eventWithPopulatedData = await Event.findById(eventId).populate([
+      { path: 'requiredSkills', populate: { path: 'category' } },
+      { path: 'volunteers' }
+    ]);
+
+    return res.status(200).json(eventWithPopulatedData);
+  }
+
+  return res.status(404).send({ error: 'Error: ONG or Event not found' });
+};
