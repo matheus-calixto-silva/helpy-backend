6cced3361c89271ca63422af6cf90b2f47721ddf
Matheus Calixto <matheuscalixto8@gmail.com>
2024-03-06 13:45:34 -0300

    feat(controllers/users): added getOngEventById and addUserToEvent to users routes

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -7,6 +7,8 @@ import { updateUserSkills } from '../useCases/users/updateUserSkills';
 import { getUserById } from '../useCases/users/getUserById';
 import { updateUser } from '../useCases/users/updateUser';
 import { listAllEvents } from '../useCases/users/listAllEvents';
+import { getOngEventById } from '../useCases/ongs/getOngEventById';
+import { addUserToEvent } from '../useCases/users/addUserToEvent';
 
 import { auth } from '../utils/middleware';
 import { upload } from '../utils/helpers';
@@ -17,6 +19,8 @@ usersRouter.get('/users', auth, listUsers);
 
 usersRouter.get('/users/events/all', auth, listAllEvents);
 
+usersRouter.get('/users/events/:eventId', auth, getOngEventById);
+
 usersRouter.post('/users', upload.single('photo'), createUser);
 
 usersRouter.get('/users/:userId', auth, getUserById);
@@ -26,3 +30,5 @@ usersRouter.put('/users/:userId', auth, updateUser);
 usersRouter.patch('/users/:userId', auth, updateUserSkills);
 
 usersRouter.delete('/users/:userId', auth, removeUser);
+
+usersRouter.patch('/users/:userId/events/:eventId', auth, addUserToEvent);
