8bbea463f92945cf57a48df512a4ad735ee99e75
Matheus Calixto <matheuscalixto8@gmail.com>
2024-03-06 13:55:34 -0300

    feat(swagger.ts): updated swagger.ts definitions

--- swagger.ts
+++ swagger.ts
@@ -54,7 +54,71 @@ const doc = {
       created_at: '2023-03-29T19:28:31.002Z',
       updated_at: '2023-03-29T19:28:31.002Z'
     },
-  }
+    UpdatedUser: {
+      _id: '63e52122b00d0c03dfc5304d',
+      firstname: 'Jack',
+      lastname: 'Doe',
+      username: 'jack_doe',
+      email: 'jack_doe@mail.com',
+      phone: '5581988888888',
+      skills: [
+        {
+          skill: {
+            _id: '6419f3540d17fe8c1b530ab6',
+            name: 'Desenvolvimento React',
+            category: {
+              _id: '6419f19a0d17fe8c1b530aa8',
+              name: 'Programação',
+              description: 'Habilidades relacionadas a desenvolvimento de software',
+              created_at: '2023-03-21T18:04:10.044Z',
+              updated_at: '2023-03-21T18:04:10.044Z'
+            },
+            created_at: '2023-03-21T18:11:32.357Z',
+            updated_at: '2023-03-21T18:11:32.357Z'
+          },
+          _id: '6424915edd50adc8b28f4dd7'
+        }
+      ],
+      created_at: '2023-03-29T19:28:31.002Z',
+      updated_at: '2023-03-29T19:28:31.002Z'
+    },
+    UserUpdatedSkills: {
+      skills: [
+        {
+          skill: {
+            _id: '6419f3670d17fe8c1b530aba',
+            name: 'Desenvolvimento NodeJs',
+            category: {
+              _id: '6419f19a0d17fe8c1b530aa8',
+              name: 'Programação',
+              description: 'Habilidades relacionadas a desenvolvimento de software',
+              created_at: '2023-03-21T18: 04: 10.044Z',
+              updated_at: '2023-03-21T18: 04: 10.044Z'
+            },
+            created_at: '2023-03-21T18: 11: 51.401Z',
+            updated_at: '2023-03-21T18: 11: 51.401Z'
+          },
+          _id: '642c6089b725de9847f41a8c'
+        },
+        {
+          skill: {
+            _id: '6419f3540d17fe8c1b530ab7',
+            name: 'Desenvolvimento React',
+            category: {
+              _id: '6419f19a0d17fe8c1b530aa9',
+              name: 'Programação',
+              description: 'Habilidades relacionadas a desenvolvimento de software',
+              created_at: '2023-03-21T18: 04: 10.044Z',
+              updated_at: '2023-03-21T18: 04: 10.044Z'
+            },
+            created_at: '2023-03-21T18: 11: 32.358Z',
+            updated_at: '2023-03-21T18: 11: 32.358Z'
+          },
+          _id: '6427876941022d82f3aa8754'
+        }
+      ]
+    },
+  },
 };
 
 swaggerAutogen()(outputFile, endpointsFiles, doc);
