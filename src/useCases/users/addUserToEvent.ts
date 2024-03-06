89151be3a0d09c31cd82c873947b0d5cb17c7a4a
Matheus Calixto <matheuscalixto8@gmail.com>
2024-03-06 12:08:53 -0300

    refactor(usecases/users): moved addUserToEvent method to users useCases

--- src/useCases/users/addUserToEvent.ts
+++ src/useCases/users/addUserToEvent.ts
@@ -1,12 +1,9 @@
 import { Request, Response } from 'express';
 
-import { User } from '../../models/user';
 import { Event } from '../../models/event';
-import { Ong } from '../../models/ong';
+import { User } from '../../models/user';
 
-import { IUser } from '../../types';
-import { IEvent } from '../../types';
-import { IOng } from '../../types';
+import { IEvent, IUser } from '../../types';
 
 
 const checkMaxVolunteers = (event: IEvent) => {
@@ -23,18 +20,17 @@ const checkIfUserIsAlredyRegistered = (user: IUser, event: IEvent) => {
 const checkIfUserHasRequiredSkill = (user: IUser, event: IEvent) => {
   const convertedRequiredSkills = event.requiredSkills.map(skill => skill.toString());
   const allUserSkillsIds = user.skills.map(skill => skill._id.toString());
-
   return allUserSkillsIds.some(skill => convertedRequiredSkills.includes(skill));
 };
 
 export const addUserToEvent = async (req: Request, res: Response) => {
-  const { ongId, eventId, userId } = req.params;
+  const { eventId, userId } = req.params;
 
-  const ong = await Ong.findById(ongId) as IOng;
   const event = await Event.findById(eventId) as IEvent;
   const user = await User.findById(userId) as IUser;
 
-  if (ong && event && user) {
+
+  if (event && user) {
 
     if (checkMaxVolunteers(event)) {
       return res.status(401).json({ error: 'Event is full' });
@@ -48,17 +44,17 @@ export const addUserToEvent = async (req: Request, res: Response) => {
       return res.status(401).json({ error: 'User is alredy registered to this event' });
     }
 
-    const updatedOngVolunteers = [...event.volunteers, user._id];
-    await Event.findByIdAndUpdate(eventId, { volunteers: updatedOngVolunteers }, { new: true });
+    const updatedEventVolunteers = [...event.volunteers, user._id];
+    await Event.findByIdAndUpdate(eventId, { volunteers: updatedEventVolunteers }, { new: true });
 
-    const ongWithPopulatedData = await Ong.findById(ongId).populate({
-      path: 'events',
-      populate: { path: 'requiredSkills' }
-    });
+    const eventWithPopulatedData = await Event.findById(eventId).populate([
+      { path: 'requiredSkills', populate: { path: 'category' } },
+      { path: 'volunteers' }
+    ]);
 
-    return res.status(200).json(ongWithPopulatedData?.events);
+    return res.status(200).json(!eventWithPopulatedData);
   } else {
-    return res.status(404).json({ error: 'Ong, Event or User not found' });
+    return res.status(404).json({ error: 'Event or User not found' });
   }
 };
 
