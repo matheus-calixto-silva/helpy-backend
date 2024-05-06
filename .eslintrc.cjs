d11085b01da1618097133785e5eeccfff26f2348
Matheus Calixto <matheuscalixto8@gmail.com>
2024-05-06 19:38:10 -0300

    chore: adds configuration to resolve TypeScript file imports
    Adds 'import/resolver' setting to ESLint to resolve imports from TypeScript (.ts) files.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,11 @@ module.exports = {
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+      },
+    },
     'import/extensions': [
       'error',
       'ignorePackages',
