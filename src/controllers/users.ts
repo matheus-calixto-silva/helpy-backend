7860fcda990cb838a02e780518483972f8feac93
Matheus Calixto <matheuscalixto8@gmail.com>
2024-05-07 22:08:22 -0300

    refactor(controllers/users): update imports to use relative paths
    Updates the imports in the file to use relative paths instead of absolute paths. This improves
    readability and makes the code easier to maintain.

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 
-import { getOngEventById } from '../useCases/ongs/getOngEventById';
-import { addUserToEvent } from '../useCases/users/addUserToEvent';
-import { createUser } from '../useCases/users/createUser';
-import { getUserById } from '../useCases/users/getUserById';
-import { listAllEvents } from '../useCases/users/listAllEvents';
-import { listUsers } from '../useCases/users/listUsers';
-import { removeUser } from '../useCases/users/removeUser';
-import { updateUser } from '../useCases/users/updateUser';
-import { updateUserSkills } from '../useCases/users/updateUserSkills';
+import { getOngEventById } from '@useCases/ongs/getOngEventById';
+import { addUserToEvent } from '@useCases/users/addUserToEvent';
+import { createUser } from '@useCases/users/createUser';
+import { getUserById } from '@useCases/users/getUserById';
+import { listAllEvents } from '@useCases/users/listAllEvents';
+import { listUsers } from '@useCases/users/listUsers';
+import { removeUser } from '@useCases/users/removeUser';
+import { updateUser } from '@useCases/users/updateUser';
+import { updateUserSkills } from '@useCases/users/updateUserSkills';
 
-import { auth } from '../middlewares/autheticationMiddleware';
-import { upload } from '../utils/helpers';
+import { auth } from '@middlewares/autheticationMiddleware';
+import { upload } from '@utils/helpers';
 
 export const usersRouter = Router();
 
