20bd7a909a4e91f559c0809479d1f94bf6f462bb
Matheus Calixto <matheuscalixto8@gmail.com>
2024-05-07 23:29:33 -0300

    refactor(usecases/categories): update imports to use relative paths
    Updates the imports in the file to use relative paths instead of absolute paths. This improves
    readability and makes the code easier to maintain.

--- src/useCases/categories/createCategory.ts
+++ src/useCases/categories/createCategory.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-import { Category } from '../../models/category';
+import { Category } from '@models/category';
 
 export const createCategory = async (req: Request, res: Response) => {
   const { name, description } = req.body;
