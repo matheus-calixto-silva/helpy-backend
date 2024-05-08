3e6cf85b6bc9f16f565be8665643f4794a76948e
Matheus Calixto <matheuscalixto8@gmail.com>
2024-05-07 22:23:56 -0300

    style(models/admin): apply eslint + prettier styles to admin.ts
    Apply eslint and prettier code styling to the admin.ts file to ensure consistent code formatting and
    improve code readability.

--- src/models/admin.ts
+++ src/models/admin.ts
@@ -1,66 +1,69 @@
 import { model, Schema } from 'mongoose';
 
-const adminSchema = new Schema({
-  firstname: {
-    type: String,
-    trim: true,
-    required: [true, 'Nome é obrigatório'],
-    minLength: 3,
-  },
-  lastname: {
-    type: String,
-    trim: true,
-    required: [true, 'Sobrenome é obrigatório'],
-    minLength: 3,
-  },
-  username: {
-    type: String,
-    trim: true,
-    required: [true, 'Nome de usuário é obrigatório'],
-    minLength: 3,
-  },
-  passwordHash: {
-    type: String,
-    trim: true,
-    required: [true, 'Senha é obrigatório'],
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: [true, 'Email é obrigatório'],
-    validate: {
-      validator: (v: string) => {
-        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-          v
-        );
+const adminSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      trim: true,
+      required: [true, 'Nome é obrigatório'],
+      minLength: 3,
+    },
+    lastname: {
+      type: String,
+      trim: true,
+      required: [true, 'Sobrenome é obrigatório'],
+      minLength: 3,
+    },
+    username: {
+      type: String,
+      trim: true,
+      required: [true, 'Nome de usuário é obrigatório'],
+      minLength: 3,
+    },
+    passwordHash: {
+      type: String,
+      trim: true,
+      required: [true, 'Senha é obrigatório'],
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, 'Email é obrigatório'],
+      validate: {
+        validator: (v: string) => {
+          return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            v,
+          );
+        },
+        message: 'o email informado não é válido!',
       },
-      message: 'o email informado não é válido!',
+    },
+    phone: {
+      type: String,
+      trim: true,
+      required: [true, 'Número de telefone é obrigatório'],
+      validate: {
+        validator: (v: string) => {
+          return /^\s*(\d{2}|\d{0})[-. ]?(\d{5}|\d{4})[-. ]?(\d{4})[-. ]?\s*$/.test(
+            v,
+          );
+        },
+        message: 'o número de telefone não é válido!',
+      },
+    },
+    profilePic: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    role: {
+      type: String,
+      trim: true,
+      required: true,
     },
   },
-  phone: {
-    type: String,
-    trim: true,
-    required: [true, 'Número de telefone é obrigatório'],
-    validate: {
-      validator: (v: string) => {
-        return /^\s*(\d{2}|\d{0})[-. ]?(\d{5}|\d{4})[-. ]?(\d{4})[-. ]?\s*$/.test(
-          v
-        );
-      },
-      message: 'o número de telefone não é válido!',
-    },
-  },
-  profilePic: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  role: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } },
+);
 
 adminSchema.set('toJSON', {
   transform: (_document, returnedObject) => {
