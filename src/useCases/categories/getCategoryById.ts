f6ebed9b6deb100c7903ac685054a3903737c0be
Matheus Calixto <matheuscalixto8@gmail.com>
2024-05-07 23:32:45 -0300

    refactor(usecases/categories): update imports to use relative paths and apply eslint + prettier
    Updates the imports in the file to use relative paths instead of absolute paths. This improves
    readability and makes the code easier to maintain and apply eslint and prettier code styling to the
    admin.ts file to ensure consistent code formatting and improve code readability.

--- src/useCases/categories/getCategoryById.ts
+++ src/useCases/categories/getCategoryById.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 
-import { Category } from '../../models/category';
+import { Category } from '@models/category';
 
-export const getCategoryById = async(req: Request, res: Response) => {
+export const getCategoryById = async (req: Request, res: Response) => {
   const { categoryId } = req.params;
   const category = await Category.findById(categoryId);
 
