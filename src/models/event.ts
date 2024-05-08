f3d778eb3019946d310f32e54a3386a939515f73
Matheus Calixto <matheuscalixto8@gmail.com>
2024-05-07 22:32:45 -0300

    style(models/event): apply eslint + prettier styles to event.ts
    Apply eslint and prettier code styling to the event.ts file to ensure consistent code formatting and
    improve code readability.

--- src/models/event.ts
+++ src/models/event.ts
@@ -1,76 +1,85 @@
 import { model, Schema } from 'mongoose';
 import { IEvent } from '../types';
 
-const eventSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: [true, 'Nome é obrigatório'],
-    minLength: 5
-  },
-  address: {
-    street: {
+const eventSchema = new Schema(
+  {
+    name: {
       type: String,
       trim: true,
-      required: [true, 'Rua é obrigatória']
+      required: [true, 'Nome é obrigatório'],
+      minLength: 5,
     },
-    city: {
+    address: {
+      street: {
+        type: String,
+        trim: true,
+        required: [true, 'Rua é obrigatória'],
+      },
+      city: {
+        type: String,
+        trim: true,
+        required: [true, 'Cidade é obrigatória'],
+      },
+      uf: {
+        type: String,
+        trim: true,
+        required: [true, 'UF é obrigatória'],
+        length: 2,
+      },
+      latitude: {
+        type: String,
+        required: [true, 'Latitude é obrigatória'],
+      },
+      longitude: {
+        type: String,
+        required: [true, 'Longitude é obrigatória'],
+      },
+    },
+    description: {
       type: String,
       trim: true,
-      required: [true, 'Cidade é obrigatória']
+      required: [true, 'Descrição do evento é obrigatório'],
+      minLength: 5,
     },
-    uf: {
+    date: {
+      type: Date,
+      required: [true, 'Data do evento é obrigatório'],
+    },
+    requiredSkills: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Skill',
+          required: true,
+        },
+      ],
+    },
+    maxVolunteers: {
+      type: Number,
+      required: [true, 'Número de voluntários é obrigatório'],
+    },
+    volunteers: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+          required: function (this: IEvent) {
+            return (
+              this.volunteers.length >= this.maxVolunteers &&
+              'Máximo de voluntários para esse evento foi atingido'
+            );
+          },
+        },
+      ],
+    },
+    eventPic: {
       type: String,
       trim: true,
-      required: [true, 'UF é obrigatória'],
-      length: 2,
+      required: true,
     },
-    latitude: {
-      type: String,
-      required: [true, 'Latitude é obrigatória']
-    },
-    longitude: {
-      type: String,
-      required: [true, 'Longitude é obrigatória']
-    }
   },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, 'Descrição do evento é obrigatório'],
-    minLength: 5,
-  },
-  date: {
-    type: Date,
-    required: [true, 'Data do evento é obrigatório']
-  },
-  requiredSkills: {
-    type: [{
-      type: Schema.Types.ObjectId,
-      ref: 'Skill',
-      required: true
-    }],
-  },
-  maxVolunteers: {
-    type: Number,
-    required: [true, 'Número de voluntários é obrigatório']
-  },
-  volunteers: {
-    type: [{
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: function (this: IEvent) {
-        return this.volunteers.length >= this.maxVolunteers
-          && 'Máximo de voluntários para esse evento foi atingido';
-      }
-    }],
-  },
-  eventPic: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } },
+);
 
 eventSchema.set('toJSON', {
   transform: (_document, returnedObject) => {
