576e381a3cc145dd25c9d3db3ac953ffdbe92e4f
Matheus Calixto <matheuscalixto8@gmail.com>
2024-05-07 23:44:11 -0300

    refactor(usecases/categories): update imports to use relative paths and apply eslint + prettier
    Updates the imports in the file to use relative paths instead of absolute paths. This improves
    readability and makes the code easier to maintain and apply eslint and prettier code styling to the
    removeCategory.ts file to ensure consistent code formatting and improve code readability.

--- src/useCases/categories/removeCategory.ts
+++ src/useCases/categories/removeCategory.ts
@@ -1,16 +1,20 @@
 import { Request, Response } from 'express';
 
-import { Category } from '../../models/category';
-import { Skill } from '../../models/skill';
+import { Category } from '@models/category';
+import { Skill } from '@models/skill';
 
 export const removeCategory = async (req: Request, res: Response) => {
   const { categoryId } = req.params;
   const category = await Category.findById(categoryId);
   const skills = await Skill.find();
-  const skillUsingCategory = skills.find(skill => skill.category?.toString() === category?._id.toString());
+  const skillUsingCategory = skills.find(
+    (skill) => skill.category?.toString() === category?._id.toString(),
+  );
 
   if (skillUsingCategory) {
-    return res.status(401).json({ error: 'This category is used by one or more skills' });
+    return res
+      .status(401)
+      .json({ error: 'This category is used by one or more skills' });
   }
 
   await Category.findByIdAndDelete(categoryId);
