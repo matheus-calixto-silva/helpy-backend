05042f3544ac8f7d02bd6c70a246734a61c66500
Matheus Calixto <matheuscalixto8@gmail.com>
2024-05-09 13:16:22 -0300

    refactor(usecases/skills): update imports to use relative paths and apply eslint + prettier
    Updates the imports in the file to use relative paths instead of absolute paths. This improves
    readability and makes the code easier to maintain and apply eslint and prettier code styling to the
    listSkills.ts and updateSkills.ts files to ensure consistent code formatting and improve code
    readability.

--- src/useCases/skills/listSkills.ts
+++ src/useCases/skills/listSkills.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 
-import { Skill } from '../../models/skill';
+import { Skill } from '@models/skill';
 
-export const listSkills = async(_req: Request, res: Response) => {
+export const listSkills = async (_req: Request, res: Response) => {
   const skills = await Skill.find().populate('category');
 
   return res.status(200).json(skills);
--- src/useCases/skills/updateSkill.ts
+++ src/useCases/skills/updateSkill.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from 'express';
 
-import { Skill } from '../../models/skill';
+import { Skill } from '@models/skill';
 
 export const updateSkill = async (req: Request, res: Response) => {
   const { skillId } = req.params;
   const updates = req.body;
 
-  const updatedSkill = await Skill.findByIdAndUpdate(skillId, updates, { new: true });
+  const updatedSkill = await Skill.findByIdAndUpdate(skillId, updates, {
+    new: true,
+  });
 
   if (!updatedSkill) {
     return res.status(404).send({ error: 'Category not found' });
