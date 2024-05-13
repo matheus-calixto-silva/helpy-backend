a8966d1798645a996b256b99bdc8cd018ee61919
Matheus Calixto <matheuscalixto8@gmail.com>
2024-05-13 16:43:56 -0300

    fix(getcategorybyid): add error handling for category retrieval
    This commit adds error handling to the getCategoryById function to ensure that if the category with
    the specified ID is not found, a 404 status code with an error message is returned.

--- src/useCases/categories/getCategoryById.ts
+++ src/useCases/categories/getCategoryById.ts
@@ -6,5 +6,9 @@ export const getCategoryById = async (req: Request, res: Response) => {
   const { categoryId } = req.params;
   const category = await Category.findById(categoryId);
 
-  return res.status(200).json(category);
+  if (category) {
+    return res.status(200).json(category);
+  }
+
+  return res.status(404).send({ error: 'Error: Category not found' });
 };
